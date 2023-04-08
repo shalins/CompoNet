c5976e90919e8db9449c880bda72e86c986c6f98
Shalin Shah <shalin.shah@matician.com>
2023-04-08 15:45:50 -0700

    plot: fixed aspect ratio

--- boost/client/tailwind.config.js
+++ boost/client/tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    height: {
+      screen: "calc(100vh - 6rem)",
+      plot: "calc(100vh - 28rem)",
+    },
     extend: {},
   },
   plugins: [],
